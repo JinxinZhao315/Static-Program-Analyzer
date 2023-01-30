7f264d8f910aeedf9977159ba09390eddc7c6054
MarcTzh <marcustan26@hotmail.com>
2023-01-30 22:55:04 +0800

    Add basic parsing line by line

--- Team15/Code15/src/spa/src/Parser.cpp
+++ Team15/Code15/src/spa/src/Parser.cpp
@@ -2,13 +2,38 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
 
 using namespace std;
 
 #include "PKB.h"
-#include "TNode.h"
 
-int Parse () {
+int Parse (string filename) {
+    cout << "File path: " << filename << endl;
 
-	return 0;
+    ifstream file(filename);
+
+    if (!file.is_open()) {
+        cout << "Failed to open file: " << filename << endl;
+        return 0;
+    }
+
+    string line;
+    // Parse the SIMPLE program line by line
+    while (getline(file, line)) {
+        cout << line << endl;
+
+    }
+
+    file.close();
+    return 0;
+}
+
+// Only for testing purposes
+int main(int argc, char* argv[]) {
+    // For early testing this filepath is temporarily hard coded
+    string filename = "Team15/Tests15/Sample_source.txt";
+    Parse(filename);
+
+    return 0;
 }
