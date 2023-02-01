5d1b1828c4144b263d4b5d00a4b0b85eb56de4f0
Aditya Banerjee <aditya_banerjee@icloud.com>
2023-02-01 15:23:56 +0800

    add tokenise method

--- Team15/Code15/src/spa/src/Tokeniser.cpp
+++ Team15/Code15/src/spa/src/Tokeniser.cpp
@@ -1,14 +1,56 @@
 #include<stdio.h>
-//#include "Tokens.h"
+#include "Tokens.h"
 #include <iostream>
-#include <string>
 #include <vector>
-#include <fstream>
 #include <map>
+#include <fstream>
 
 using namespace std;
 
 map<int, int> nesting_level;
+
+bool findToken(std::string s) {
+    auto it = Tokens::TOKEN_MAP.find(s);
+    if (it != Tokens::TOKEN_MAP.end()) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool emptyToken(std::string token) {
+    if(token == "" || token == " ") {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+std::vector<std::string> pushToken(std::vector<std::string> tokens, std::string token) {
+    if(!emptyToken(token)) {
+        tokens.push_back(token);
+    }
+    return tokens;
+}
+
+std::vector<std::string> tokenise(std::string line) {
+    std::vector<std::string> tokens;
+    std::string currentToken = "";
+    for(char c : line) {
+        std:string s(1, c);
+        if(findToken(s)) {
+            tokens = pushToken(tokens, currentToken);
+            tokens = pushToken(tokens, s);
+            currentToken = "";
+        } else if (c == ' ') {
+            tokens = pushToken(tokens, currentToken);
+            currentToken = "";
+        } else {
+            currentToken += c;
+        }
+    }
+    return tokens;
+}
 
 void processFile(std::ifstream &file) {
     string curr_line;
@@ -31,4 +73,11 @@ void processFile(std::ifstream &file) {
         nesting_level[line_number] = curr_level;
     }
     cout << nesting_level[line_number];
+}
+
+int main() {
+    std::vector<std::string> tokens = tokenise("procedure computeAverage() { read x; print x; } ");
+    for (std::string token : tokens) {
+        cout << token << endl;
+    }
 }
