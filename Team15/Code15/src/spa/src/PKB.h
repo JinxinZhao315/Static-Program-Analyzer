a0326a6f3408eca44db1680bcdffa5d6730660ba
Galen Cheung <galencheung97@gmail.com>
2023-02-03 12:58:34 +0800

    Add virtual functions in PKB header file

--- Team15/Code15/src/spa/src/PKB.h
+++ Team15/Code15/src/spa/src/PKB.h
@@ -4,18 +4,50 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+#include "EntityTable.h"
+#include "AbstractionTable.h"
+#include "StatementTable.h"
 
 using namespace std;
-typedef short PROC;
-
-class TNode;
-
-class VarTable;  // no need to #include "VarTable.h" as all I need is pointer
 
 class PKB {
 public:
-	static VarTable* varTable; 
-	static int setProcToAST(PROC p, TNode* r);
-	static TNode* getRootAST (PROC p);
+	virtual void addProc(Procedure* proc);
 
+	virtual void addStmt(Statement* stmt);
+
+	virtual void addVar(Variable* var);
+
+	virtual void addConst(Constant* constant);
+
+	virtual void addFollows(Statement* leader, Statement* follower);
+
+	virtual void addParent(Statement* parent, Statement* child);
+
+	virtual void addStmtUsesVar(Statement* stmt, Variable* var);
+
+	virtual void addProcUsesVar(Procedure* proc, Variable* var);
+
+	virtual void addStmtModifiesVar(Statement* stmt, Variable* var);
+
+	virtual void addProcModifiesVar(Procedure* proc, Variable* var);
+
+	virtual Procedure* getProc(const ProcedureName& procName);
+
+	virtual Statement* getStmt(const StatementNumber& stmtNum);
+
+	virtual Variable* getVar(const VariableName& varName);
+
+	virtual Constant* getConst(const ConstantName& constName);
+
+	virtual unordered_set<ProcedureName> getAllProcNames();
+
+	virtual unordered_set<StatementNumber> getAllStmtNums();
+
+	virtual unordered_set<VariableName> getAllVarNames();
+
+	virtual unordered_set<ConstantName> getAllConstNames();
 };
