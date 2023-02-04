f40d4ecd104fc37bcf3107f8898e80fe5d935ba2
qinguorui2001 <qinguorui666@gmail.com>
2023-02-04 17:40:21 +0800

    Commit  combine Results

--- Team15/Code15/src/spa/src/relationship_handlers/ClauseHandler.cpp
+++ Team15/Code15/src/spa/src/relationship_handlers/ClauseHandler.cpp
@@ -4,9 +4,33 @@
 
 #include "ClauseHandler.h"
 
-ClauseHandler::ClauseHandler(PKB pkb, ResultTable resultTable) {
+ClauseHandler::ClauseHandler(PKB& pkb) {
     this->pkb = pkb;
-    this->resultTable = resultTable;
 }
 
-// TODO: Add combineResults method
+void ClauseHandler::combineResult(ResultTable& resultTable, Result& result) {
+
+    std::string synonymName;
+    std::set<std::string> synonymSet;
+
+    std::set<std::string> intersection;
+    std::insert_iterator<std::set<std::string>> intersectIterate(intersection, intersection.begin());
+
+
+    if (result.isLeftArgSynonym()) {
+        synonymName = result.getLeftArgName();
+        synonymSet = result.getLeftArgValue();
+    }
+    else {
+        synonymName = result.getRighttArgName();
+        synonymSet = result.getRightArgValue();
+    }
+    if (resultTable.isKeyPresent(synonymName)) {
+        std::set<std::string> currSynonymValue = resultTable.getValueFromKey(synonymName);
+        set_intersection(synonymSet.begin(), synonymSet.end(), currSynonymValue.begin(), currSynonymValue.end(), intersectIterate);
+        resultTable.insertKeyValuePair(synonymName, intersection);
+    }
+    else {
+        resultTable.insertKeyValuePair(synonymName, synonymSet);
+    }
+}
