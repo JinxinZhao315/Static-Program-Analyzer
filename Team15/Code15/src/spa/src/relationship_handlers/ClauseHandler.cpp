cffcac200224582bf02abb581ea58778d7aa8d54
qinguorui2001 <qinguorui666@gmail.com>
2023-02-04 17:49:49 +0800

    fix bug and make better combine results

--- Team15/Code15/src/spa/src/relationship_handlers/ClauseHandler.cpp
+++ Team15/Code15/src/spa/src/relationship_handlers/ClauseHandler.cpp
@@ -13,18 +13,21 @@ void ClauseHandler::combineResult(ResultTable& resultTable, Result& result) {
     std::string synonymName;
     std::set<std::string> synonymSet;
 
-    std::set<std::string> intersection;
-    std::insert_iterator<std::set<std::string>> intersectIterate(intersection, intersection.begin());
-
-
     if (result.isLeftArgSynonym()) {
         synonymName = result.getLeftArgName();
         synonymSet = result.getLeftArgValue();
+        combine(synonymName, synonymSet, resultTable);
     }
-    else {
-        synonymName = result.getRighttArgName();
+    if (result.isRightArgSynonym()) {
+        synonymName = result.getRightArgName();
         synonymSet = result.getRightArgValue();
+        combine(synonymName, synonymSet, resultTable);
     }
+   
+}
+void combine(std::string synonymName, std::set<std::string>synonymSet, ResultTable& resultTable) {
+    std::set<std::string> intersection;
+    std::insert_iterator<std::set<std::string>> intersectIterate(intersection, intersection.begin());
     if (resultTable.isKeyPresent(synonymName)) {
         std::set<std::string> currSynonymValue = resultTable.getValueFromKey(synonymName);
         set_intersection(synonymSet.begin(), synonymSet.end(), currSynonymValue.begin(), currSynonymValue.end(), intersectIterate);
