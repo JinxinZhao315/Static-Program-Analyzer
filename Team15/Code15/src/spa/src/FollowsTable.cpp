0387c4d938213b1f957ddab85df57e7851c6b536
Galen Cheung <galencheung97@gmail.com>
2023-02-06 01:37:32 +0800

    Add Follows Table

---
+++ Team15/Code15/src/spa/src/FollowsTable.cpp
@@ -0,0 +1,50 @@
+#include "FollowsTable.h"
+
+FollowsTable::FollowsTable() = default;
+
+void FollowsTable::addFollows(int leader, int follower) {
+	addFollower(leader, follower);
+	addLeader(leader, follower);
+}
+
+void FollowsTable::addFollower(int leader, int follower) {
+	auto pair = leaderToFollowerMap.find(leader);
+	if (pair == leaderToFollowerMap.end()) {
+		leaderToFollowerMap[leader] = follower;
+	}
+}
+
+void FollowsTable::addLeader(int leader, int follower) {
+	auto pair = followerToLeaderMap.find(follower);
+	if (pair == followerToLeaderMap.end()) {
+		followerToLeaderMap[follower] = leader;
+	}
+}
+
+int FollowsTable::getFollower(int leader) {
+	auto pair = leaderToFollowerMap.find(leader);
+	if (pair == leaderToFollowerMap.end()) {
+		return -1;
+	}
+	return pair->second;
+}
+
+int FollowsTable::getLeader(int follower) {
+	auto pair = followerToLeaderMap.find(follower);
+	if (pair == followerToLeaderMap.end()) {
+		return -1;
+	}
+	return pair->second;
+}
+
+bool FollowsTable::inRelationship(int leaderNumber, int followerNumber) {
+	auto pair = leaderToFollowerMap.find(leaderNumber);
+	if (pair == leaderToFollowerMap.end()) {
+		return false;
+	}
+	auto follower = pair->second;
+	if (follower == followerNumber) {
+		return true;
+	}
+	return false;
+}
---
+++ Team15/Code15/src/spa/src/FollowsTable.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <stdio.h>
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+class FollowsTable {
+public:
+	FollowsTable();
+
+	void addFollows(int leader, int follower);
+
+	int getFollower(int leader);
+
+	int getLeader(int follower);
+
+	bool inRelationship(int leader, int follower);
+
+	void addFollower(int leader, int follower);
+
+	void addLeader(int leader, int follower);
+
+private:
+	std::map<int, int> leaderToFollowerMap;
+	std::map<int, int> followerToLeaderMap;
+};
