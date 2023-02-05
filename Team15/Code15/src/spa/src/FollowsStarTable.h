833a29d9a3d40f6b5b1eb506c3a560b98f00bd55
Galen Cheung <galencheung97@gmail.com>
2023-02-06 01:38:01 +0800

    Add Follows Star Table

---
+++ Team15/Code15/src/spa/src/FollowsStarTable.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <stdio.h>
+#include <iostream>
+#include <map>
+#include <set>
+
+using namespace std;
+
+class FollowsStarTable {
+public:
+	FollowsStarTable();
+
+	void addFollowsStar(int leader, std::set<int> followers);
+
+	std::set<int> getFollowers(int leader);
+
+	std::set<int> getLeaders(int follower);
+
+	bool inRelationship(int leader, int follower);
+
+	void addFollowers(int leader, std::set<int> followers);
+
+	void addLeaders(int leader, std::set<int> followers);
+private:
+	std::map<int, std::set<int>> leaderToFollowersMap;
+	std::map<int, std::set<int>> followerToLeadersMap;
+};
