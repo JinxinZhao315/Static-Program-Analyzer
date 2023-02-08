26c5974147772ff7e12fddaf944cea70783c923a
qinguorui2001 <qinguorui666@gmail.com>
2023-02-08 18:18:27 +0800

    comment out

--- Team15/Code15/src/spa/src/QPS/relationship_handlers/ModifiesHandler.cpp
+++ Team15/Code15/src/spa/src/QPS/relationship_handlers/ModifiesHandler.cpp
@@ -1,12 +1,12 @@
-#include "ModifiesHandler.h"
-
-<<<<<<< HEAD
-//ModifiesHandler(PKB& pkb) : ClauseHandler(pkb) {}
+//#include "ModifiesHandler.h"
+//
+//
+//ModifiesHandler::ModifiesHandler(PKB& pkb) : ClauseHandler(pkb) {}
 //
 //Result ModifiesHandler::evalModifies(SuchThatClause suchThatClause, ResultTable& resultTable, std::multimap<std::string, std::string>& varTable) {
-//	if (suchThatClause.get()) {
-//	
-//	}
+////	if (suchThatClause.getRelationShip()) {
+////
+////	}
 //}
 //
 //std::set<int> ModifiesHandler::getModifiesFromPKB(bool isStar, string type, string arg) {
@@ -16,20 +16,4 @@
 //bool ModifiesHandler::getIsModifiesFromPKB(bool isStar, string leftArg, string rightArg) {
 //
 //}
-=======
-ModifiesHandler::ModifiesHandler(PKB& pkb) : ClauseHandler(pkb) {}
 
-Result ModifiesHandler::evalModifies(SuchThatClause suchThatClause, ResultTable& resultTable, std::multimap<std::string, std::string>& varTable) {
-//	if (suchThatClause.getRelationShip()) {
-//
-//	}
-}
-
-std::set<int> ModifiesHandler::getModifiesFromPKB(bool isStar, string type, string arg) {
-
-}
-
-bool ModifiesHandler::getIsModifiesFromPKB(bool isStar, string leftArg, string rightArg) {
-
-}
->>>>>>> d54c8469b4beb6a1aede62e86a93f7fa8618a823
--- Team15/Code15/src/spa/src/QPS/relationship_handlers/ParentHandler.h
+++ Team15/Code15/src/spa/src/QPS/relationship_handlers/ParentHandler.h
@@ -1,34 +1,3 @@
-//
-// Created by Qin Guorui on Feb 7.
-//
-<<<<<<< HEAD
-//
-//#include "ClauseHandler.h"
-//#include "Result.h"
-//#include "ResultTable.h"
-//#include "SuchThatClause.h"
-//#include <map>
-//#include <string>
-//#include <set>
-//#ifndef SPA_PARENTSARHANDLER_H
-//#define SPA_PARENTSARHANDLER_H
-//
-//
-//class ParentHandler : public ClauseHandler {
-//public:
-//    ParentHandler(PKB& pkb);
-//    Result evalParentStar(bool isStar, SuchThatClause suchThatClause, ResultTable& resultTable, std::multimap<std::string, std::string>& varTable);
-//    std::set<int> getParentFromPKB(bool isStar, string type, string arg);
-//    bool getIsParentFromPKB(bool isStar, string leftArg, string rightArg);
-//private:
-//    string GET_PARENT = "get_parent";
-//    string GET_CHILD = "get_child";
-//};
-//
-//
-//#endif //SPA_PARENTSARHANDLER_H
-=======
-
 #include "ClauseHandler.h"
 #include "Result.h"
 #include "ResultTable.h"
@@ -53,4 +22,3 @@ private:
 
 
 #endif //SPA_PARENTSARHANDLER_H
->>>>>>> d54c8469b4beb6a1aede62e86a93f7fa8618a823
