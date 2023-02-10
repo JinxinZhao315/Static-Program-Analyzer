069508aa09ccc9e8a639d02d488ffc0961c71da0
Jay-luozhijie <77223932+Jay-luozhijie@users.noreply.github.com>
2023-02-10 20:18:38 +0800

    Add UsesHandler

---
+++ Team15/Code15/src/spa/src/QPS/relationship_handlers/UsesHandler.h
@@ -0,0 +1,18 @@
+//#include "ClauseHandler.h"
+//#include "Result.h"
+//#include "ResultTable.h"
+//#include "QPS/SuchThatClause.h"
+//#include <map>
+//#include <string>
+//#include <set>
+//
+//#pragma once
+//
+//
+//class UsesHandler : public ClauseHandler {
+//public:
+//    UsesHandler(PKB& pkb);
+//    Result evalUses(SuchThatClause suchThatClause, ResultTable& resultTable, std::multimap<std::string, std::string>& synonymTable);
+//
+//private:
+//};
