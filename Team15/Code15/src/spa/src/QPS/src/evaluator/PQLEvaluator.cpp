9b13c0974230136e5e8f6dac453e6d7d2c0f30a0
qinguorui2001 <qinguorui666@gmail.com>
2023-02-12 20:09:21 +0800

    add modifies test

--- Team15/Code15/src/spa/src/QPS/src/evaluator/PQLEvaluator.cpp
+++ Team15/Code15/src/spa/src/QPS/src/evaluator/PQLEvaluator.cpp
@@ -33,9 +33,6 @@ std::string PQLEvaluator::evaluate(Query query)
             {
 
                 resultTable.resetKeySetEmpty(selectedVarName);
-
-                resultTable.deleteKeyValuePair(selectedVarName);
-                resultTable.insertKeyValuePair(selectedVarName, {});
                 isEarlyExit = true;
 
                 break;
@@ -55,7 +52,7 @@ std::string PQLEvaluator::evaluate(Query query)
             usesHandler.combineResult(resultTable, result);
         }*/
 
-       /* if (relationship == "Parent" || relationship == "Parent*") {
+       if (relationship == "Parent" || relationship == "Parent*") {
             ParentHandler parentHandler = ParentHandler(pkb);
             bool isStar = relationship == "Parent" ? false : true;
             Result result = parentHandler.evalParentStar(isStar, suchThatCl, resultTable, synonymTable);
@@ -64,36 +61,36 @@ std::string PQLEvaluator::evaluate(Query query)
                 break;
             }
             parentHandler.combineResult(resultTable, result);
-        }*/
-        //if (relationship == "Modifies") {
-        //    Result result;
-        //    std::string leftArg = suchThatCl.getLeftArg();
-        //    std::string leftType = Utility::getReferenceType(leftArg);
-        //    ModifiesPHandler modifiesPHandler = ModifiesPHandler(pkb);
-        //    ModifiesSHandler modifiesSHandler = ModifiesSHandler(pkb);
+        }
+        if (relationship == "Modifies") {
+            Result result;
+            std::string leftArg = suchThatCl.getLeftArg();
+            std::string leftType = Utility::getReferenceType(leftArg);
+            //ModifiesPHandler modifiesPHandler = ModifiesPHandler(pkb);
+            ModifiesSHandler modifiesSHandler = ModifiesSHandler(pkb);
 
-        //    if (leftType == Utility::QUOTED_IDENT || synonymTable.find(leftArg)->second == "procedure") {
-        //        modifiesPHandler = ModifiesPHandler(pkb);
-        //        result = modifiesPHandler.evalModifiesP(suchThatCl, resultTable, synonymTable);
-        //    }
-        //    else {
-        //        modifiesSHandler = ModifiesSHandler(pkb);
-        //        result = modifiesSHandler.evalModifiesS(suchThatCl, resultTable, synonymTable);
-        //    }
+            if (leftType == Utility::QUOTED_IDENT || synonymTable.find(leftArg)->second == "procedure") {
+                //modifiesPHandler = ModifiesPHandler(pkb);
+                //result = modifiesPHandler.evalModifiesP(suchThatCl, resultTable, synonymTable);
+            }
+            else {
+                modifiesSHandler = ModifiesSHandler(pkb);
+                result = modifiesSHandler.evalModifiesS(suchThatCl, resultTable, synonymTable);
+            }
 
 
-        //    if (result.isResultTrue() == false) {
-        //        resultTable.resetKeySetEmpty(selectedVarName);
-        //        break;
-        //    }
-        //    if (leftType == Utility::QUOTED_IDENT || synonymTable.find(leftArg)->second == "procedure") {
-        //        modifiesPHandler.combineResult(resultTable, result);
-        //    }
-        //    else {
-        //        modifiesSHandler.combineResult(resultTable, result);
-        //    }
+            if (result.isResultTrue() == false) {
+                resultTable.resetKeySetEmpty(selectedVarName);
+                break;
+            }
+            if (leftType == Utility::QUOTED_IDENT || synonymTable.find(leftArg)->second == "procedure") {
+                //modifiesPHandler.combineResult(resultTable, result);
+            }
+            else {
+                modifiesSHandler.combineResult(resultTable, result);
+            }
 
-        //}
+        }
     }
     for (PatternClause patternCl : patternVec)
     {
