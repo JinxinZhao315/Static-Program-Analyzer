886e3f1b8c594c7f26660342264f0568f27d1828
Galen Cheung <galencheung97@gmail.com>
2023-02-14 00:01:17 +0800

    Add Parent Table

---
+++ Team15/Code15/src/spa/src/ParentTable.cpp
@@ -0,0 +1,54 @@
+#include "ParentTable.h"
+
+ParentTable::ParentTable() = default;
+
+void ParentTable::addParentChild(int parent, int child) {
+	addChild(parent, child);
+	addParent(parent, child);
+}
+
+int ParentTable::getChild(int parent) {
+	auto pair = parentToChildMap.find(parent);
+	if (pair == parentToChildMap.end()) {
+		return -1;
+	}
+	return pair->second;
+}
+
+int ParentTable::getParent(int child) {
+	auto pair = childToParentMap.find(child);
+	if (pair == childToParentMap.end()) {
+		return -1;
+	}
+	return pair->second;
+}
+
+bool ParentTable::inRelationship(int parentNumber, int childNumber) {
+	auto pair = parentToChildMap.find(parentNumber);
+	if (pair == parentToChildMap.end()) {
+		return false;
+	}
+	auto child = pair->second;
+	if (child == childNumber) {
+		return true;
+	}
+	return false;
+}
+
+bool ParentTable::isEmpty() {
+	return parentToChildMap.empty() && childToParentMap.empty();
+}
+
+void ParentTable::addChild(int parent, int child) {
+	auto pair = parentToChildMap.find(parent);
+	if (pair == parentToChildMap.end()) {
+		parentToChildMap[parent] = child;
+	}
+}
+
+void ParentTable::addParent(int parent, int child) {
+	auto pair = childToParentMap.find(child);
+	if (pair == childToParentMap.end()) {
+		childToParentMap[child] = parent;
+	}
+}
