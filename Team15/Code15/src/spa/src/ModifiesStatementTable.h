a207312ffa29e67e3728f192028b09b1a4e7a33b
Galen Cheung <galencheung97@gmail.com>
2023-02-14 00:03:06 +0800

    Add Modifies Statement Table

---
+++ Team15/Code15/src/spa/src/ModifiesStatementTable.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <unordered_map>
+#include <set>
+#include <string>
+
+using namespace std;
+
+class ModifiesStatementTable {
+public:
+	ModifiesStatementTable();
+
+	void addModifiesStatement(int statement, std::set<std::string> variables);
+
+	std::set<std::string> getVariables(int statement);
+
+	std::set<int> getStatements(std::string variable);
+
+	bool inRelationship(int statementNumber, std::string variableName);
+
+	bool isEmpty();
+
+	void addVariables(int statement, std::set<std::string> variables);
+
+	void addStatements(int statement, std::set<std::string> variables);
+
+private:
+	std::unordered_map<int, std::set<std::string>> statementToVariablesMap;
+	std::unordered_map<std::string, std::set<int>> variableToStatementsMap;
+};
