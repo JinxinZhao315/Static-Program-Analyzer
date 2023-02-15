5a0951457b412699dbb747e45cba804f87e4ea9f
Galen Cheung <galencheung97@gmail.com>
2023-02-15 18:41:57 +0800

    Update Modifies Stmt Table to add a whole map

--- Team15/Code15/src/spa/src/ModifiesStatementTable.h
+++ Team15/Code15/src/spa/src/ModifiesStatementTable.h
@@ -12,6 +12,8 @@ public:
 
 	void addModifiesStatement(int statement, std::set<std::string> variables);
 
+	void addAllModifiesStatement(std::unordered_map<int, std::set<std::string>> statementToVariables);
+
 	std::set<std::string> getVariables(int statement);
 
 	std::set<int> getStatements(std::string variable);
@@ -23,6 +25,8 @@ public:
 	void addVariables(int statement, std::set<std::string> variables);
 
 	void addStatements(int statement, std::set<std::string> variables);
+
+	void flipStatementToVariables(std::unordered_map<int, std::set<std::string>> statementToVariables);
 
 private:
 	std::unordered_map<int, std::set<std::string>> statementToVariablesMap;
