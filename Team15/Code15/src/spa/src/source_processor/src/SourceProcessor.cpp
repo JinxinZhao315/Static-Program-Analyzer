faf0466c00c7090e3b7330b445d761476d9ce913
MarcTzh <marcustan26@hotmail.com>
2023-02-17 15:32:49 +0800

    comment out main function in SP

--- Team15/Code15/src/spa/src/source_processor/src/SourceProcessor.cpp
+++ Team15/Code15/src/spa/src/source_processor/src/SourceProcessor.cpp
@@ -33,12 +33,12 @@ void SourceProcessor::extract(const vector<Line>& program) {
     extractor->extract(program);
 }
 
-int main() {
-    cout << "Creating source processor" << endl;
-    string fileName = "../../../../../../Tests15/Sample_source.txt";
-
-    SourceProcessor *sourceProcessor = new SourceProcessor();
-    sourceProcessor->parseProgram(fileName);
-    sourceProcessor->printParsedProgram(); //Optional
-    return 0;
-}
+//int main() {
+//    cout << "Creating source processor" << endl;
+//    string fileName = "../../../../../../Tests15/Sample_source.txt";
+//
+//    SourceProcessor *sourceProcessor = new SourceProcessor();
+//    sourceProcessor->parseProgram(fileName);
+//    sourceProcessor->printParsedProgram(); //Optional
+//    return 0;
+//}
