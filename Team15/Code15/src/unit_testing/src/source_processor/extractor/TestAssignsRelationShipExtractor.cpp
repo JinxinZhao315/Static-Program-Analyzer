696c6a320628b4e4dcd16baaf60180cb276bc5f0
MarcTzh <marcustan26@hotmail.com>
2023-02-19 12:03:34 +0800

    fix QPS import statements that causes build to fail

--- Team15/Code15/src/unit_testing/src/source_processor/extractor/TestAssignsRelationShipExtractor.cpp
+++ Team15/Code15/src/unit_testing/src/source_processor/extractor/TestAssignsRelationShipExtractor.cpp
@@ -2,9 +2,9 @@
 #include "Constants.hpp"
 #include "source_processor/include/extractor/AssignsRelationshipExtractor.h"
 
-//TEST_CASE("extractAssignmentRS_noAssignments") {
-//    set<string> variables;
-//    auto result = extractAssignmentRS(readPoint, variables);
-//    unordered_map<string, set<Line>> expected;
-//    REQUIRE(result == expected);
-//}
+TEST_CASE("extractAssignmentRS_noAssignments") {
+    set<string> variables;
+    auto result = extractAssignmentRS(readPoint, variables);
+    unordered_map<string, set<Line>> expected;
+    REQUIRE(result == expected);
+}
--- Team15/Code15/src/unit_testing/src/test_QPS/TestPQLSemanticCheck.cpp
+++ Team15/Code15/src/unit_testing/src/test_QPS/TestPQLSemanticCheck.cpp
@@ -1,5 +1,5 @@
 #include "QPS/include/tokenizer/PQLPreprocessor.h"
-#include "Query.h"
+#include "QPS/include/model/Query.h"
 #include "exceptions/PQLSyntaxError.h"
 #include "exceptions/PQLSemanticError.h"
 
