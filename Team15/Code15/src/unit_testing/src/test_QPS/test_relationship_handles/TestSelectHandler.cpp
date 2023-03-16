7d097c590114cfd132056ba966548de20f4dcab1
Jay-luozhijie <77223932+Jay-luozhijie@users.noreply.github.com>
2023-03-16 15:18:47 +0800

    Add more select handler tests

--- Team15/Code15/src/unit_testing/src/test_QPS/test_relationship_handles/TestSelectHandler.cpp
+++ Team15/Code15/src/unit_testing/src/test_QPS/test_relationship_handles/TestSelectHandler.cpp
@@ -227,7 +227,22 @@ TEST_CASE("Select Handler test 5 multi synonyms") {
 }
 
 TEST_CASE("Select Handler test 6 multi synonyms with attribute") {
+    std::set<std::string> retStr1 = testSelect("stmt s1, s2; Select <s1, s2.stmt#> such that Follows(s1, s2)");
+    std::set<std::string> expected1 = { "1 2", "2 3", "3 4", "4 5", "5 6" };
+    REQUIRE(retStr1 == expected1);
 
+    std::set<std::string> retStr2 = testSelect("stmt s1, s2; Select <s1.stmt#, s2>");
+    std::set<std::string> expected2 = { "1 1","1 2","1 3","1 4","1 5","1 6",
+    "2 1","2 2","2 3","2 4","2 5","2 6",
+    "3 1","3 2","3 3","3 4","3 5", "3 6",
+    "4 1","4 2","4 3","4 4","4 5","4 6",
+    "5 1","5 2","5 3","5 4", "5 5", "5 6",
+    "6 1","6 2","6 3","6 4","6 5","6 6" };
+    REQUIRE(retStr2 == expected2);
+
+    std::set<std::string> retStr3 = testSelect("stmt s1, s2; Select <s1.stmt#> such that Follows(s1, s2)");
+    std::set<std::string> expected3 = { "1", "2", "3", "4", "5" };
+    REQUIRE(retStr1 == expected1);
 }
 
 TEST_CASE("Select Handler test 3: multiple synonyms") {
