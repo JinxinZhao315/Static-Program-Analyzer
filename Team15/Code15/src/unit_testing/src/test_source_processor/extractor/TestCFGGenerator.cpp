2e51813f779ccecd9625ed80086b748d9b0f2897
Aditya Banerjee <aditya_banerjee@icloud.com>
2023-03-18 15:49:25 +0800

    add tests for CFG generator and next

--- Team15/Code15/src/unit_testing/src/test_source_processor/extractor/TestCFGGenerator.cpp
+++ Team15/Code15/src/unit_testing/src/test_source_processor/extractor/TestCFGGenerator.cpp
@@ -28,33 +28,311 @@ void compareCFG(set<CFGNode*> cfg1, set<CFGNode*> cfg2) {
 TEST_CASE("Empty Program") {
     const vector<Line>& lines = {};
     const set<CFGNode*> expected = {};
-    generateCFG(lines);
-//    REQUIRE(expected == result);
+    map<int, set<int>> result = generateCFG(lines);
+    REQUIRE(result, Equals(expected));
 }
 
 TEST_CASE("No nesting") {
     const set<CFGNode *> expected = {};
-    generateCFG(mainProgram);
-//    compareCFG(expected, result);
+    map<int, set<int>> result = generateCFG(mainProgram);
+    REQUIRE_THAT(result, Equals(expected));
 }
 
 TEST_CASE("whileLoop") {
     const set<CFGNode*> expected = {};
-    generateCFG(whileLoopInProcedure);
-//    compareCFG(expected, result);
+    map<int, set<int>> result = generateCFG(whileLoopInProcedure);
+    REQUIRE_THAT(result, Equals(expected));
 }
 
 
 TEST_CASE("whileLoopWithLineBeforeAfter") {
     const set<CFGNode*> expected = {};
-    generateCFG(whileLoopInProcedureWithLinesBeforeAfter);
-//    compareCFG(expected, result);
+    map<int, set<int>> result = generateCFG(whileLoopInProcedureWithLinesBeforeAfter);
+    REQUIRE_THAT(result, Equals(expected));
 }
 
 TEST_CASE("ifElseInProcedure") {
     const set<CFGNode*> expected = {};
-    generateCFG(ifElseInProcedure);
-//    compareCFG(expected, result);
+    map<int, set<int>> result = generateCFG(ifElseInProcedure);
+    REQUIRE_THAT(result, Equals(expected));
+}
+
+TEST_CASE("ifNoElseNoLinesBeforeAndAfter") {
+    const vector<Line>& lines = {
+            Line({"procedure", "B", "{"}, "procedure"),
+            Line(1, {"if", "(", "x", "==", "1", ")", "then", "{"}, "if"),
+            Line(2, {"x", "=", "x", "+", "1", ";"}, "="),
+            Line({"}"}, "}"),
+            Line({"}"}, "}"),
+    };
+
+    unordered_map<int, set<int>> expected = {
+            {1, {2}},
+    };
+
+    map<int, set<int>> result = generateCFG(lines);
+    REQUIRE_THAT(result, Equals(expected));
+}
+
+TEST_CASE("ifNoElseWithLinesBeforeAndAfter") {
+    const vector<Line>& lines = {
+            Line({"procedure", "B", "{"}, "procedure"),
+            Line(1, {"x", "=", "x", "+", "1", ";"}, "="),
+            Line(2, {"if", "(", "x", "==", "1", ")", "then", "{"}, "if"),
+            Line(3, {"x", "=", "x", "+", "1", ";"}, "="),
+            Line({"}"}, "}"),
+            Line(4, {"x", "=", "x", "+", "1", ";"}, "="),
+            Line({"}"}, "}"),
+    };
+
+    unordered_map<int, set<int>> expected = {
+            {1, {2}},
+            {2, {3, 4}}
+    };
+
+    map<int, set<int>> result = generateCFG(lines);
+    REQUIRE_THAT(result, Equals(expected));
+}
+
+TEST_CASE("ifAndElseNoLinesBeforeAndAfter") {
+    const vector<Line>& lines = {
+            Line({"procedure", "B", "{"}, "procedure"),
+            Line(1, {"if", "(", "x", "==", "1", ")", "then", "{"}, "if"),
+            Line(2, {"x", "=", "x", "+", "1", ";"}, "="),
+            Line({"}"}, "}"),
+            Line({"else", "{"}, "else"),
+            Line(2, {"x", "=", "x", "+", "1", ";"}, "="),
+            Line({"}"}, "}"),
+            Line({"}"}, "}"),
+    };
+
+    unordered_map<int, set<int>> expected = {
+            {1, {2, 3}},
+            {2, {3}}
+    };
+
+    map<int, set<int>> result = generateCFG(lines);
+    REQUIRE_THAT(result, Equals(expected));
+}
+
+TEST_CASE("ifAndElseWithLinesBeforeAndAfter") {
+    const vector<Line>& lines = {
+            Line({"procedure", "B", "{"}, "procedure"),
+            Line(1, {"x", "=", "x", "+", "1", ";"}, "="),
+            Line(2, {"if", "(", "x", "==", "1", ")", "then", "{"}, "if"),
+            Line(3, {"x", "=", "x", "+", "1", ";"}, "="),
+            Line({"}"}, "}"),
+            Line({"else", "{"}, "else"),
+            Line(4, {"x", "=", "x", "+", "1", ";"}, "="),
+            Line({"}"}, "}"),
+            Line(5, {"x", "=", "x", "+", "1", ";"}, "="),
+            Line({"}"}, "}"),
+    };
+
+    unordered_map<int, set<int>> expected = {
+            {1, {2}},
+            {2, {3, 4}},
+            {3, {5}},
+            {4, {5}},
+    };
+
+    map<int, set<int>> result = generateCFG(lines);
+    REQUIRE_THAT(result, Equals(expected));
+}
+
+TEST_CASE("extractNextRelationship_whileNoLinesBeforeAndAfter") {
+    const vector<Line>& lines = {
+            Line({"procedure", "B", "{"}, "procedure"),
+            Line(1, {"while", "(", "y", "==", "1", ")", "{"}, "while"),
+            Line(2, {"x", "=", "x", "+", "1", ";"}, "="),
+            Line({"}"}, "}"),
+            Line({"}"}, "}"),
+    };
+
+    unordered_map<int, set<int>> expected = {
+            {1, {2}}
+    };
+
+    map<int, set<int>> result = generateCFG(lines);
+    REQUIRE_THAT(result, Equals(expected));
+}
+
+TEST_CASE("extractNextRelationship_whileWithLinesBeforeAndAfter") {
+    const vector<Line>& lines = {
+            Line({"procedure", "B", "{"}, "procedure"),
+            Line(1, {"x", "=", "x", "+", "1", ";"}, "="),
+            Line(2, {"while", "(", "y", "==", "1", ")", "{"}, "while"),
+            Line(3, {"x", "=", "x", "+", "1", ";"}, "="),
+            Line({"}"}, "}"),
+            Line(4, {"x", "=", "x", "+", "1", ";"}, "="),
+            Line({"}"}, "}"),
+    };
+
+    unordered_map<int, set<int>> expected = {
+            {1, {2}},
+            {2, {3, 4}},
+            {3, {2, 4}}
+    };
+
+    map<int, set<int>> result = generateCFG(lines);
+    REQUIRE_THAT(result, Equals(expected));
+}
+
+TEST_CASE("extractNextRelationship_whileNestedInWhile") {
+    const vector<Line>& lines = {
+            Line({"procedure", "B", "{"}, "procedure"),
+            Line(1, {"x", "=", "x", "+", "1", ";"}, "="),
+            Line(2, {"while", "(", "y", "==", "1", ")", "{"}, "while"),
+            Line(3, {"while", "(", "y", "==", "1", ")", "{"}, "while"),
+            Line(4, {"x", "=", "x", "+", "1", ";"}, "="),
+            Line({"}"}, "}"),
+            Line(5, {"x", "=", "x", "+", "1", ";"}, "="),
+            Line({"}"}, "}"),
+            Line(6, {"x", "=", "x", "+", "1", ";"}, "="),
+            Line({"}"}, "}"),
+    };
+
+    unordered_map<int, set<int>> expected = {
+            {1, {2}},
+            {2, {3, 6}},
+            {3, {4, 5}},
+            {4, {3, 5}},
+            {5, {2, 6}},
+    };
+
+    map<int, set<int>> result = generateCFG(lines);
+    REQUIRE_THAT(result, Equals(expected));
+}
+
+TEST_CASE("extractNextRelationship_ifWithElseNestedInWhile") {
+    const vector<Line>& lines = {
+            Line({"procedure", "B", "{"}, "procedure"),
+            Line(1, {"x", "=", "x", "+", "1", ";"}, "="),
+            Line(2, {"while", "(", "y", "==", "1", ")", "{"}, "while"),
+            Line(3, {"if", "(", "y", "==", "1", ")", "{"}, "if"),
+            Line(4, {"x", "=", "x", "+", "1", ";"}, "="),
+            Line({"}"}, "}"),
+            Line({"else", "{"}, "else"),
+            Line(5, {"x", "=", "x", "+", "1", ";"}, "="),
+            Line({"}"}, "}"),
+            Line(6, {"x", "=", "x", "+", "1", ";"}, "="),
+            Line({"}"}, "}"),
+            Line(7, {"x", "=", "x", "+", "1", ";"}, "="),
+            Line({"}"}, "}"),
+    };
+
+    unordered_map<int, set<int>> expected = {
+            {1, {2}},
+            {2, {3, 7}},
+            {3, {4, 5}},
+            {4, {6}},
+            {5, {6}},
+            {6, {2, 7}}
+    };
+
+    map<int, set<int>> result = generateCFG(lines);
+    REQUIRE_THAT(result, Equals(expected));
+}
+
+
+TEST_CASE("extractNextRelationship_ifWithElseWhileNestedInEachBranch") {
+    const vector<Line>& lines = {
+            Line({"procedure", "B", "{"}, "procedure"),
+            Line(1, {"if", "(", "x", "==", "1", ")", "then", "{"}, "if"),
+            Line(2, {"while", "(", "x", "==", "1", ")", "then", "{"}, "while"),
+            Line(3, {"x", "=", "x", "+", "1", ";"}, "="),
+            Line({"}"}, "}"),
+            Line({"}"}, "}"),
+            Line({"else", "{"}, "else"),
+            Line(4, {"while", "(", "x", "==", "1", ")", "then", "{"}, "while"),
+            Line(5, {"x", "=", "x", "+", "1", ";"}, "="),
+            Line({"}"}, "}"),
+            Line({"}"}, "}"),
+            Line({"}"}, "}"),
+    };
+
+    unordered_map<int, set<int>> expected = {
+            {1, {2, 4}},
+            {2, {3}},
+            {3, {2}},
+            {4, {5}},
+            {5, {4}},
+    };
+
+    map<int, set<int>> result = generateCFG(lines);
+    REQUIRE_THAT(result, Equals(expected));
+}
+
+TEST_CASE("extractNextRelationship_whileWithNestedIfWithElse") {
+    const vector<Line>& lines = {
+            Line({"procedure", "B", "{"}, "procedure"),
+            Line(1, {"while", "(", "y", "==", "1", ")", "{"}, "while"),
+            Line(2, {"x", "=", "x", "+", "1", ";"}, "="),
+            Line(3, {"if", "(", "y", "==", "1", ")", "{"}, "if"),
+            Line(4, {"x", "=", "x", "+", "1", ";"}, "="),
+            Line({"}"}, "}"),
+            Line({"else", "{"}, "else"),
+            Line(5, {"x", "=", "x", "+", "1", ";"}, "="),
+            Line({"}"}, "}"),
+            Line(6, {"x", "=", "x", "+", "1", ";"}, "="),
+            Line({"}"}, "}"),
+            Line(7, {"x", "=", "x", "+", "1", ";"}, "="),
+            Line({"}"}, "}"),
+    };
+
+    unordered_map<int, set<int>> expected = {
+            {1, {2, 7}},
+            {2, {3}},
+            {3, {4, 5}},
+            {4, {6}},
+            {5, {6}},
+            {6, {1, 7}}
+    };
+
+    map<int, set<int>> result = generateCFG(lines);
+    REQUIRE_THAT(result, Equals(expected));
+}
+
+TEST_CASE("extractNextRelationship_ifWithElseEachBranchHasNestedWhileWithNestedIfWithElse") {
+    const vector<Line> &lines = {
+            Line({"procedure", "B", "{"}, "procedure"),
+            Line(1, {"if", "(", "x", "==", "1", ")", "then", "{"}, "if"),
+            Line(2, {"while", "(", "x", "==", "1", ")", "then", "{"}, "while"),
+            Line(3, {"if", "(", "y", "==", "1", ")", "{"}, "if"),
+            Line(4, {"x", "=", "x", "+", "1", ";"}, "="),
+            Line({"}"}, "}"),
+            Line({"else", "{"}, "else"),
+            Line(5, {"x", "=", "x", "+", "1", ";"}, "="),
+            Line({"}"}, "}"),
+            Line({"}"}, "}"),
+            Line({"}"}, "}"),
+            Line({"else", "{"}, "else"),
+            Line(6, {"while", "(", "x", "==", "1", ")", "then", "{"}, "while"),
+            Line(7, {"if", "(", "y", "==", "1", ")", "{"}, "if"),
+            Line(8, {"x", "=", "x", "+", "1", ";"}, "="),
+            Line({"}"}, "}"),
+            Line({"else", "{"}, "else"),
+            Line(9, {"x", "=", "x", "+", "1", ";"}, "="),
+            Line({"}"}, "}"),
+            Line({"}"}, "}"),
+            Line({"}"}, "}"),
+            Line({"}"}, "}"),
+    };
+
+    unordered_map<int, set<int>> expected = {
+            {1, {2, 6}},
+            {2, {3}},
+            {3, {4, 5}},
+            {4, {2}},
+            {5, {2}},
+            {6, {7}},
+            {7, {8, 9}},
+            {8, {6}},
+            {9, {6}}
+    };
+
+    map<int, set<int>> result = generateCFG(lines);
+    REQUIRE_THAT(result, Equals(expected));
 }
 
 TEST_CASE("ifWithNestedIfAndElseInThenAndNestedWhileInElse") {
@@ -73,7 +351,14 @@ TEST_CASE("ifWithNestedIfAndElseInThenAndNestedWhileInElse") {
             Line({"}"}, "}"),
             Line({"}"}, "}"),
     };
+
+    unordered_map<int, set<int>> expected = {
+            {1, {2, 5}},
+            {2, {3, 4}},
+            {5, {6}},
+            {6, {5}}
+    };
+
     map<int, set<int>> result = generateCFG(lines);
-//    REQUIRE_THAT(result, Equals(expected)); //TODO: write expected
-//    compareCFG(expected, result);
+    REQUIRE_THAT(result, Equals(expected));
 }
