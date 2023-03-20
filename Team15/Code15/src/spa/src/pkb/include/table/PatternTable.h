a08a7cc908795d438f0bf47e84c84c1463292998
Galen Cheung <galencheung97@gmail.com>
2023-03-20 11:58:15 +0800

    Remove unused methods in Pattern Table

--- Team15/Code15/src/spa/src/pkb/include/table/PatternTable.h
+++ Team15/Code15/src/spa/src/pkb/include/table/PatternTable.h
@@ -19,10 +19,10 @@ public:
 				std::vector<std::string> expr = line.getTokens();
 				mapStmtToVar(stmtNum, var);
 				mapVarToStmts(var, stmtNum);
-				mapStmtToOneExpr(stmtNum, expr);
-				mapOneExprToStmts(expr, stmtNum);
-				mapVarToOneExpr(var, expr);
-				mapOneExprToVars(expr, var);
+				mapStmtToExprs(stmtNum, expr);
+				mapExprToStmts(expr, stmtNum);
+				mapVarToExprs(var, expr);
+				mapExprToVars(expr, var);
 			}
 		}
 	}
@@ -136,17 +136,7 @@ private:
 		}
 	}
 
-	inline void mapStmtToExprs(int assignStmtNum, std::set<std::vector<std::string>> rhsExprs) {
-		auto pair = stmtToExprsMap.find(assignStmtNum);
-		if (pair == stmtToExprsMap.end()) {
-			stmtToExprsMap[assignStmtNum] = rhsExprs;
-		}
-		else {
-			pair->second.insert(rhsExprs.begin(), rhsExprs.end());
-		}
-	}
-
-	inline void mapStmtToOneExpr(int assignStmtNum, std::vector<std::string> rhsExpr) {
+	inline void mapStmtToExprs(int assignStmtNum, std::vector<std::string> rhsExpr) {
 		auto pair = stmtToExprsMap.find(assignStmtNum);
 		if (pair == stmtToExprsMap.end()) {
 			stmtToExprsMap[assignStmtNum] = { rhsExpr };
@@ -156,19 +146,7 @@ private:
 		}
 	}
 
-	inline void mapExprToStmts(std::set<std::vector<std::string>> rhsExprs, int assignStmtNum) {
-		for (std::vector<std::string> expr : rhsExprs) {
-			auto pair = exprToStmtsMap.find(expr);
-			if (pair == exprToStmtsMap.end()) {
-				exprToStmtsMap[expr] = { assignStmtNum };
-			}
-			else {
-				pair->second.insert(assignStmtNum);
-			}
-		}
-	}
-
-	inline void mapOneExprToStmts(std::vector<std::string> rhsExpr, int assignStmtNum) {
+	inline void mapExprToStmts(std::vector<std::string> rhsExpr, int assignStmtNum) {
 		auto pair = exprToStmtsMap.find(rhsExpr);
 		if (pair == exprToStmtsMap.end()) {
 			exprToStmtsMap[rhsExpr] = { assignStmtNum };
@@ -178,17 +156,7 @@ private:
 		}
 	}
 
-	inline void mapVarToExprs(std::string lhsVarName, std::set<std::vector<std::string>> rhsExprs) {
-		auto pair = varToExprsMap.find(lhsVarName);
-		if (pair == varToExprsMap.end()) {
-			varToExprsMap[lhsVarName] = rhsExprs;
-		}
-		else {
-			pair->second.insert(rhsExprs.begin(), rhsExprs.end());
-		}
-	}
-
-	inline void mapVarToOneExpr(std::string lhsVarName, std::vector<std::string> rhsExpr) {
+	inline void mapVarToExprs(std::string lhsVarName, std::vector<std::string> rhsExpr) {
 		auto pair = varToExprsMap.find(lhsVarName);
 		if (pair == varToExprsMap.end()) {
 			varToExprsMap[lhsVarName] = { rhsExpr };
@@ -198,19 +166,7 @@ private:
 		}
 	}
 
-	inline void mapExprToVars(std::set<std::vector<std::string>> rhsExprs, std::string lhsVarName) {
-		for (std::vector<std::string> expr : rhsExprs) {
-			auto pair = exprToVarsMap.find(expr);
-			if (pair == exprToVarsMap.end()) {
-				exprToVarsMap[expr] = { lhsVarName };
-			}
-			else {
-				pair->second.insert(lhsVarName);
-			}
-		}
-	}
-
-	inline void mapOneExprToVars(std::vector<std::string> rhsExpr, std::string lhsVarName) {
+	inline void mapExprToVars(std::vector<std::string> rhsExpr, std::string lhsVarName) {
 		auto pair = exprToVarsMap.find(rhsExpr);
 		if (pair == exprToVarsMap.end()) {
 			exprToVarsMap[rhsExpr] = { lhsVarName };
