38af2f8644f09cfa251cc2732daf3e9cbca3eff4
MarcTzh <marcustan26@hotmail.com>
2023-03-24 14:47:36 +0800

    WIP: rename variables for clarity

--- Team15/Code15/src/spa/src/source_processor/src/extractor/CFGGenerator.cpp
+++ Team15/Code15/src/spa/src/source_processor/src/extractor/CFGGenerator.cpp
@@ -9,8 +9,8 @@ unordered_map<int, set<int>> generateCFG(const vector<Line>& program) {
     // intermediate data structures for each procedure
     stack<pair<int, string>> nestingStack;
     vector<int> nodesToJoin;
+    bool prevLineHasNumber = false;
     int prevLineNumber = -1;
-    int prevValidLineNumber = -1;
     int currMaxLineNumber = 0;
     int nextLineNumber;
     for (int i = 0; i < program.size(); i++) {
@@ -19,20 +19,20 @@ unordered_map<int, set<int>> generateCFG(const vector<Line>& program) {
         string lineType = line.getType();
         currMaxLineNumber = max(currMaxLineNumber, lineNumber);
         nextLineNumber = currMaxLineNumber + 1;
-        bool isPrevLineValid = prevLineNumber != -1 && prevLineNumber != lineNumber;
+        bool isPrevLineValid = prevLineHasNumber && prevLineNumber != lineNumber;
         if (lineType == "procedure") {
             nestingStack = stack<pair<int, string>>(); // clear stack
             nodesToJoin.clear();
-            prevLineNumber = -1;
+            prevLineHasNumber = false;
             continue;
         } else if (lineType == "if" || lineType == "while") {
             if (isPrevLineValid) {
-                cfg[prevValidLineNumber].insert(lineNumber);
+                cfg[prevLineNumber].insert(lineNumber);
             }
             nestingStack.emplace(lineNumber, lineType);
             nodesToJoin.clear(); // clear nodesToJoin for new nesting level
             cfg[lineNumber].insert(nextLineNumber);
-            prevLineNumber = -1;
+            prevLineHasNumber = false;
         } else if (lineType == "else") {
             if (!nestingStack.empty()) {
                 int parentLine;
@@ -40,17 +40,17 @@ unordered_map<int, set<int>> generateCFG(const vector<Line>& program) {
                 tie(parentLine, parentType) = nestingStack.top();
                 cfg[parentLine].insert(nextLineNumber);
             }
-            nodesToJoin.push_back(prevValidLineNumber);
-            prevLineNumber = -1;
+            nodesToJoin.push_back(prevLineNumber);
+            prevLineHasNumber = false;
         } else if (lineType == "}") {
             if (!nestingStack.empty()) {
                 int parentLine;
                 string parentType;
                 tie(parentLine, parentType) = nestingStack.top();
                 if (parentType == "if") {
-                    nodesToJoin.push_back(prevValidLineNumber);
+                    nodesToJoin.push_back(prevLineNumber);
                 } else if (parentType == "while") {
-                    cfg[prevValidLineNumber].insert(parentLine); // link back to head of while loop
+                    cfg[prevLineNumber].insert(parentLine); // link back to head of while loop
                     // todo: put this 4 lines in correct spot
                     for (int lineToLink: nodesToJoin) {
                         cfg[lineToLink].insert(parentLine); // link all nodes to next line
@@ -69,17 +69,17 @@ unordered_map<int, set<int>> generateCFG(const vector<Line>& program) {
                 }
                 nestingStack.pop();
             }
-            prevLineNumber = -1;
+            prevLineHasNumber = false;
         } else {
             if (isPrevLineValid) {
-                cfg[prevValidLineNumber].insert(lineNumber);
+                cfg[prevLineNumber].insert(lineNumber);
             }
         }
 
 
         if (lineNumber > 0) {
+            prevLineHasNumber = true;
             prevLineNumber = lineNumber;
-            prevValidLineNumber = lineNumber;
         }
     }
 
