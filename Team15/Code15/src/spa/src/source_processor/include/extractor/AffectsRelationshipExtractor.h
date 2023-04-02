dd82e214bee7df5af14f4e195193050ead782e3c
Aditya Banerjee <aditya_banerjee@icloud.com>
2023-04-02 21:26:50 +0800

    add support for multiple wildcards for affects and affects star

--- Team15/Code15/src/spa/src/source_processor/include/extractor/AffectsRelationshipExtractor.h
+++ Team15/Code15/src/spa/src/source_processor/include/extractor/AffectsRelationshipExtractor.h
@@ -20,6 +20,13 @@ set<int> extractAffectsWithWildcard(const vector<Line>& program, int lineNum, bo
                                     const unordered_map<int, set<string>>& usesRS,
                                     bool findAffectsStar);
 
+unordered_map<int, set<int>> extractAffectsWithMultipleWildcards(const vector<Line>& program,
+                                                                 const unordered_map<int, set<int>>& cfg,
+                                                                 const set<string>& variables,
+                                                                 const unordered_map<int, set<string>>& modifiesRS,
+                                                                 const unordered_map<int, set<string>>& usesRS,
+                                                                 bool findAffectsStar);
+
 // find a path between line1 and line2, if found return as vector of ints representing that control flow
 void dfs(unordered_map<int, set<int>> cfg, int lineNum1, int lineNum2, vector<int>* path, vector<vector<int>>* paths,
          unordered_map<int, int>* visited);
