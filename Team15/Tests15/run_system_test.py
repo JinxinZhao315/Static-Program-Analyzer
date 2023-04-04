8b0b361acf56649d2529a1bdb7707bf8b516926e
Galen Cheung <galencheung97@gmail.com>
2023-04-04 12:39:55 +0800

    Update run system test py

--- Team15/Tests15/run_system_test.py
+++ Team15/Tests15/run_system_test.py
@@ -1,11 +1,17 @@
 import subprocess
 
-tests = ["affects"]
-#tests = ["assign", "bool", "calls", "follows", "general", "ifwhile", "next", "parent", "tuple", "usesmodifies", "with"]
+tests = ["affects", "assign", "bool", "calls", "follows", "general", "ifwhile", "next", "parent", "tuple", "usesmodifies", "with"]
+
+#for test in tests:
+#    source = "source-" + test + ".txt"
+#    query = "query-" + test + ".txt"
+#    out = "out-" + test + ".xml"
+#    cmd = ["autotester.exe", source, query, out]
+#    subprocess.run(cmd)
 
 for test in tests:
-    source = "source-" + test + ".txt"
-    query = "query-" + test + ".txt"
-    out = "out-" + test + ".xml"
+    source = test + "_source" + ".txt"
+    query = test + "_query" + ".txt"
+    out = test + "_out" + ".xml"
     cmd = ["autotester.exe", source, query, out]
     subprocess.run(cmd)
