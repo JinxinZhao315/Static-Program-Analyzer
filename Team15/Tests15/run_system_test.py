227552b7b8be0c2fd9387f00647d9d9f8e6e6b4f
Galen Cheung <galencheung97@gmail.com>
2023-04-04 14:15:01 +0800

    Update run_system_test.py

--- Team15/Tests15/run_system_test.py
+++ Team15/Tests15/run_system_test.py
@@ -1,21 +1,18 @@
 import subprocess
 
-#tests = ["affects", "assign", "bool", "calls", "follows", "general", "ifwhile", "next", "parent", "tuple", "usesmodifies", "with"]
-tests = []
-
-for i in range(1, 27):
-    tests.append(str(i))
-
-#for test in tests:
-#    source = "source-" + test + ".txt"
-#    query = "query-" + test + ".txt"
-#    out = "out-" + test + ".xml"
-#    cmd = ["autotester.exe", source, query, out]
-#    subprocess.run(cmd)
-
+#Run our test cases
+tests = ["affects", "assign", "bool", "calls", "follows", "general", "ifwhile", "next", "parent", "tuple", "usesmodifies", "with"]
 for test in tests:
-    source = test + "_source" + ".txt"
-    query = test + "_queries" + ".txt"
-    out = test + "_out" + ".xml"
-    cmd = ["autotester.exe", source, query, out]
-    subprocess.run(cmd)
+   source = "source-" + test + ".txt"
+   query = "query-" + test + ".txt"
+   out = "out-" + test + ".xml"
+   cmd = ["autotester.exe", source, query, out]
+   subprocess.run(cmd)
+
+#Run Jared's test cases
+# for i in range(1, 28):
+#     source = str(i) + "_source" + ".txt"
+#     query = str(i) + "_queries" + ".txt"
+#     out = str(i) + "_out" + ".xml"
+#     cmd = ["autotester.exe", source, query, out]
+#     subprocess.run(cmd)
