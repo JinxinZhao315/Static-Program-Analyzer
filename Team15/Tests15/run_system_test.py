ed2a8799cc4409e264abab0960e3d9db359d5d93
Galen Cheung <galencheung97@gmail.com>
2023-04-04 12:58:09 +0800

    Update system tests

--- Team15/Tests15/run_system_test.py
+++ Team15/Tests15/run_system_test.py
@@ -1,6 +1,10 @@
 import subprocess
 
-tests = ["affects", "assign", "bool", "calls", "follows", "general", "ifwhile", "next", "parent", "tuple", "usesmodifies", "with"]
+#tests = ["affects", "assign", "bool", "calls", "follows", "general", "ifwhile", "next", "parent", "tuple", "usesmodifies", "with"]
+tests = []
+
+for i in range(1, 27):
+    tests.append(str(i))
 
 #for test in tests:
 #    source = "source-" + test + ".txt"
@@ -11,7 +15,7 @@ tests = ["affects", "assign", "bool", "calls", "follows", "general", "ifwhile", 
 
 for test in tests:
     source = test + "_source" + ".txt"
-    query = test + "_query" + ".txt"
+    query = test + "_queries" + ".txt"
     out = test + "_out" + ".xml"
     cmd = ["autotester.exe", source, query, out]
     subprocess.run(cmd)
