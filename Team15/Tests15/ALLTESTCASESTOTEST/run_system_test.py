22fcd5f2bd4a02099e05f37145ff14360d210456
MarcTzh <marcustan26@hotmail.com>
2023-04-10 02:24:47 +0800

    change autotester python script for mac and make affects faster

--- Team15/Tests15/ALLTESTCASESTOTEST/run_system_test.py
+++ Team15/Tests15/ALLTESTCASESTOTEST/run_system_test.py
@@ -5,22 +5,22 @@ import subprocess
 
 # Please add new test cases in alphabetical order. Thanks :)
 l = [
-	"Affects",
-	"Boolean",
-	"Calls",
-	"CallsT",
-	"Follows",
-	"FollowsParent",
-	"ModifiesP",
-	"ModifiesS",
-	"Multiple",
-	"Next",
-	"OtherClauses",
-	"Parent",
-	"Pattern",
-	"PatternExtra",
-	"SystemTest_queries",
-	"Tuple",
+    "Affects",
+    "Boolean",
+    "Calls",
+    "CallsT",
+    "Follows",
+    "FollowsParent",
+    "ModifiesP",
+    "ModifiesS",
+    "Multiple",
+    "Next",
+    "OtherClauses",
+    "Parent",
+    "Pattern",
+    "PatternExtra",
+    "SystemTest_queries",
+    "Tuple",
     "UsesP",
     "UsesS",
     "With",
@@ -28,8 +28,13 @@ l = [
 
 
 for test in l:
-   source = "SystemTest_source.txt"
-   query = test + ".txt"
-   out = "../../Code15/tests/ALLTESTCASESTOTEST/out-" + test + ".xml"
-   cmd = ["../../Code15/out/build/x64-Debug/src/autotester/autotester.exe", source, query, out]
-   subprocess.run(cmd)
+	source = "SystemTest_source.txt"
+	query = test + ".txt"
+	if sys.platform == "win32": #win
+		out = "../../Code15/tests/ALLTESTCASESTOTEST/out-" + test + ".xml"
+		cmd = ["../../Code15/out/build/x64-Debug/src/autotester/autotester.exe", source, query, out]
+		subprocess.run(cmd)
+	elif sys.platform == "darwin": #mac
+		out = "../../Code15/tests/ALLTESTCASESTOTEST/out-" + test + ".xml"
+		cmd = ["../../Code15/cmake-build-debug/src/autotester/autotester", source, query, out]
+		subprocess.run(cmd)
