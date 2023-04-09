f593405132c25203ceaa96360a4ad4c0d9b60dc2
Galen Cheung <galencheung97@gmail.com>
2023-04-09 15:00:04 +0800

    Update EntityTable to include unordered map

--- Team15/Code15/src/spa/src/pkb/include/table/EntityTable.h
+++ Team15/Code15/src/spa/src/pkb/include/table/EntityTable.h
@@ -2,6 +2,7 @@
 
 #include <set>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 #include "common/include/models/Line.h"
@@ -11,12 +12,20 @@ class EntityTable {
 public:
 	inline EntityTable() = default;
 
+	inline void addEntityToSet(E entity) {
+		entitySet.insert(entity);
+	}
+
 	inline void addAllEntities(std::set<E> entities) {
 		entitySet = entities;
 	}
 
 	inline void addAllEntities(std::vector<E> entities) {
 		entityVector = entities;
+	}
+
+	inline void addEntityMapping(int entityId, E entity) {
+		entityMap[entityId] = entity;
 	}
 
 	inline std::set<E> getEntitySet() {
@@ -27,7 +36,12 @@ public:
 		return entityVector;
 	}
 
+	inline std::unordered_map<int, E> getEntityMap() {
+		return entityMap;
+	}
+
 private:
 	std::set<E> entitySet;
 	std::vector<E> entityVector;
+	std::unordered_map<int, E> entityMap;
 };
